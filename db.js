3268040ce5d13fa129a40f500535939ae49925f1
Ilia Kurtov <kurtovi@gmail.com>
2017-05-30 01:17:48 +0300

    follow review comments

--- db.js
+++ db.js
@@ -1,4 +1,33 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(":memory:");
+var uuid = require('uuid');
 
-module.exports = db;
+function create_db() {
+    db.serialize(function () {
+        db.run(
+            "CREATE TABLE IF NOT EXISTS messages (" +
+            "  id         INTEGER  PRIMARY KEY AUTOINCREMENT," +
+            "  message_id TEXT     NOT NULL," +
+            "  message    TEXT     NOT NULL," +
+            "  timestamp  DATETIME DEFAULT CURRENT_TIMESTAMP" +
+            "); "+
+            "CREATE UNIQUE INDEX id_index ON id_index(message_id, message);"
+        );
+
+        var stmt = db.prepare("INSERT INTO messages(message_id, message) VALUES (?, ?)");
+        for (var i = 0; i < 40; i++) {
+            // stmt.run(uuid.v4(), "Ipsum " + (i + 1));
+            stmt.run("a"+i, "Ipsum " + (i + 1));
+        }
+        stmt.finalize();
+
+        db.each("SELECT * FROM messages", function (err, row) {
+            console.log(row.message_id + ":  " + row.message + "  " + row.timestamp);
+        });
+    });
+}
+
+module.exports = {
+    db: db,
+    create_db_func: create_db
+};
--- routes/messages.js
+++ routes/messages.js
@@ -1,111 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db');
 
-router.get('/getAll', function (req, res, next) {
-    db.all("SELECT * FROM messages", function (err, rows) {
-        if (err) {
-            console.err(err);
-            res.status(500);
-        } else {
-            res.json(rows);
-        }
-    });
+var create = require('./messages/create');
+var get = require('./messages/get');
+
+router.get('/get', function (req, res, next) {
+    get(req, res, next);
 });
 
-router.post('/send', function (req, res, next) {
-    var message = req.body.message;
-    if (message) {
-        var sql = "INSERT INTO messages(message) VALUES (?)";
-        db.run(sql, [message], function (err, rows) {
-            if (err) {
-                console.err(err);
-                res.status(500);
-            } else {
-                res.status(200);
-            }
-            res.end();
-        });
-    } else {
-        res.json({error: "Provide message, please"});
-    }
+router.post('/create', function (req, res, next) {
+    create(req, res, next);
 });
-
-router.get('/getByOffset', function (req, res, next) {
-    var offset = parseInt(req.query.offset) || 0;
-    var limit = parseInt(req.query.limit) || 5;
-    var sql = "SELECT * FROM messages ORDER BY message_id DESC LIMIT ? OFFSET ?";
-    db.all(sql, [limit, offset], function (err, rows) {
-        if (err) {
-            console.err(err);
-            res.status(500);
-        } else {
-            res.json(rows);
-        }
-    });
-});
-
-router.get('/getByAnchor', function (req, res, next) {
-    var message_anchor = parseInt(req.query.anchor) || undefined;
-    var limit = parseInt(req.query.limit) || 5;
-
-    var direction = req.query.direction;
-    var toOldest;
-    if (direction === undefined) {
-        res.json({error: "Please, provide order of messages: direction = toNewest or toOldest"});
-        return;
-    } else if (direction === "toOldest") {
-        toOldest = true;
-    } else if (direction === "toNewest") {
-        toOldest = false;
-    } else {
-        res.json({error: "Direction should be toNewest or toOldest"});
-        return;
-    }
-
-
-    if (message_anchor !== undefined) {
-        with_anchor(res, toOldest, message_anchor, limit);
-    } else {
-        without_anchor(res, toOldest, limit);
-    }
-
-});
-
-
-function with_anchor(res, toOldest, message_anchor, limit) {
-    var anchored_sql;
-    if (toOldest) {
-        anchored_sql = "SELECT * FROM messages WHERE message_id < ?  ORDER BY message_id DESC LIMIT ?";
-    } else {
-        anchored_sql = "SELECT * FROM messages WHERE message_id > ?  ORDER BY message_id ASC LIMIT ?";
-    }
-
-    db.all(anchored_sql, [message_anchor, limit], function (err, rows) {
-        if (err) {
-            console.err(err);
-            res.status(500);
-        } else {
-            res.json(rows);
-        }
-    });
-}
-
-function without_anchor(res, toOldest, limit) {
-    var sql;
-    if (toOldest) {
-        sql = "SELECT * FROM messages ORDER BY message_id DESC LIMIT ?";
-    } else {
-        sql = "SELECT * FROM messages ORDER BY message_id ASC LIMIT ?";
-    }
-    db.all(sql, [limit], function (err, rows) {
-        if (err) {
-            console.err(err);
-            res.status(500);
-        } else {
-            res.json(rows);
-        }
-    });
-}
 
 module.exports = router;
